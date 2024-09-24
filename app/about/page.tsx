1ebe2ba97339911e62a3da17cfba7fd265bd4e90
vishnuk1609 <vishnu@glasier.in>
2024-09-24 12:43:16 +0530

    test commit

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,7 +3,13 @@ import React from 'react';
 type Props = {};
 
 const AboutPage = ({}: Props) => {
-  return <div>page</div>;
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis rem fuga soluta dolorem consequatur non neque,
+      consectetur, quos doloribus perferendis voluptates, eum dolore ab assumenda atque veritatis commodi reprehenderit
+      doloremque.
+    </div>
+  );
 };
 
 export default AboutPage;
